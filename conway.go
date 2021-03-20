e510ec82afe039f9b7c7ded74b3ab217556e5a12
thepedroferrari <pedro@pedroferrari.com>
2021-03-20 01:23:12 +0100

    Fix install

--- conway.go
+++ conway.go
@@ -1,9 +1,9 @@
 package main
-import(
-	"github.com/hajimehoshi/ebiten"
-	"image/color"
-	"math/rand"
-	"log"
+import (
+    "github.com/hajimehoshi/ebiten"
+    "image/color"
+    "math/rand"
+    "log"
 )
 const scale=2
 const WIDTH=160
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/thepedroferrari/GO-Conway
+
+go 1.16
+
+require (
+	github.com/hajimehoshi/ebiten v1.12.9
+	github.com/hajimehoshi/ebiten/v2 v2.0.6 // indirect
+)
